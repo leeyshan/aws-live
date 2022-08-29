379fc55fe87e883be8485b01ffa352b6627e76cd
leeyshan <112379539+leeyshan@users.noreply.github.com>
2022-08-29 23:03:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c1rcjuzk2mzy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leeyingshan-employee"
+customregion = "us-east-1"
